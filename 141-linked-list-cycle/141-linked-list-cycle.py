3252a71a40e201a3ce8030f7639def762a49d17f
galegoer <48871245+galegoer@users.noreply.github.com>
2025-02-08 12:48:57 -0500

    Uploading solution and readme for 141-linked-list-cycle

---
+++ 141-linked-list-cycle/141-linked-list-cycle.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        visited = {}        
+        pos = 0
+        
+        while head is not None:
+            if head in visited:
+                return True
+            else:
+                visited[head] = head.val
+            head = head.next
+            pos += 1
+        
+        return False
